7b9e29b0080172166f141c829671025ab2271afd
Rain S. Zhang <mylaluna@gmail.com>
2022-03-01 11:20:46 +1100

    Simple bank sample project

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "account" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entry" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfer" (
+  "id" bigserial PRIMARY KEY,
+  "from_accountid" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "entry" ADD FOREIGN KEY ("account_id") REFERENCES "account" ("id");
+
+ALTER TABLE "transfer" ADD FOREIGN KEY ("from_accountid") REFERENCES "account" ("id");
+
+ALTER TABLE "transfer" ADD FOREIGN KEY ("to_account_id") REFERENCES "account" ("id");
+
+CREATE INDEX ON "account" ("owner");
+
+CREATE INDEX ON "entry" ("account_id");
+
+CREATE INDEX ON "transfer" ("from_accountid");
+
+CREATE INDEX ON "transfer" ("to_account_id");
+
+CREATE INDEX ON "transfer" ("from_accountid", "to_account_id");
+
+COMMENT ON COLUMN "entry"."amount" IS 'can be negative or positive';
+
+COMMENT ON COLUMN "transfer"."amount" IS 'must be positive';
