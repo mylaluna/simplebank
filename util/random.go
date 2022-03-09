17a09016793715ff05d39b0b82eff3a8c1e351bf
Rain S. Zhang <mylaluna@gmail.com>
2022-03-09 18:34:46 +1100

    Add Money transfer API and with custom currency validator

--- util/random.go
+++ util/random.go
@@ -44,7 +44,7 @@ func RandomMoney() int64 {
 
 // RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "AUD", "RMB"}
+	currencies := []string{EUR, USD, AUD, RMB}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
